d711ba3ec58ccea231f929b337ec182dd4001f57
Elvinp329 <elvinkj@code.edu.az>
2023-06-18 02:43:21 +0400

    FirstCommit

---
+++ Task1.MVC/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace Task1.MVC.Models
+{
+	public class Student
+	{
+		public int Id { get; set; }
+		public string Firstname { get; set; }
+		public string Lastname { get; set; }
+		public int Age { get; set; }
+
+		public override string ToString()
+		{
+			return $"{Id} {Firstname} {Lastname} {Age}";
+		}
+
+	}
+}
